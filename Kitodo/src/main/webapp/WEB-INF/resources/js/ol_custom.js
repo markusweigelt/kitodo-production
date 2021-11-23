caabc65044344710e301f37381570b3e833d1c64
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2021-11-23 16:47:00 +0100

    improve media editor to support video files, small refactoring for further media types

--- Kitodo/src/main/webapp/WEB-INF/resources/js/ol_custom.js
+++ Kitodo/src/main/webapp/WEB-INF/resources/js/ol_custom.js
@@ -225,9 +225,11 @@ function addListener(element) {
 
 function initializeImage() {
     hideCanvas();
-    let image = $("#imagePreviewForm\\:mediaViewData");
-    addListener(image);
-    image[0].src = image[0].src.replace(/&uuid=[a-z0-9]+/i, "") + "&uuid=" + random(8);
+    let images = $("#imagePreviewForm\\:mediaPreviewGraphicImage");
+    if( images.length > 0 ) {
+        addListener(images);
+        images[0].src = images[0].src.replace(/&uuid=[a-z0-9]+/i, "") + "&uuid=" + random(8);
+    }
 }
 
 function changeToMapView() {
